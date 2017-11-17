b2198fb079d6f6c4bfeee866faf3f399423769fb
May <may.schiller@gmail.com>
2017-11-17 11:55:33 +0200

    update sprint2

--- js/main.js
+++ js/main.js
@@ -16,27 +16,58 @@ function scrollGetTouch() {
 
 
 //render pictures
-var gPics = [1, 2, 3, 4];
-// var gImgs = getImgs();
-// renderImgs(gImgs);
-var gImgs;
-
+var gPics = ['child.jpg', 'baby.jpg', 'girl.jpg', 'woman.jpg', 'man.jpg', 'wanka.jpg', 'american.jpg', 'red.jpg'];
+var gImgs = [];
+getImgs();
 
 function getImgs() {
 
-    gPics.forEach(function (pic) {
-        console.log('pic',pic);
-        gImgs.push(getImg(pic));
+    gPics.forEach(function (pic, idx) {
+        console.log('pic', pic);
+        gImgs.push(getImg(pic, idx));
     })
+    renderImgs();
 }
 
-getImgs();
 
-function getImg(idx) {
+function getImg(pic, idx) {
 
     return {
         id: idx,
-        url: idx + '.jpg',
+        url: pic,
         keywords: []
     }
+}
+
+function renderImgs() {
+
+    var elImgs = document.querySelector('.imgs');
+    var strHtmls = '';
+
+    gImgs.forEach(function (img, idx) {
+        var strHtml = `
+        <div><img  src="img/${img.url}"  id="i-${idx}" onclick="makeMeme(this)" ></div>
+        `
+        strHtmls += strHtml
+    });
+    elImgs.innerHTML = strHtmls;
+}
+
+
+function addImg() {
+
+    var pic = document.querySelector('.img-url').value;
+    gImgs.push(getImg(pic, gPics.length));
+    console.log('gPics', gPics);
+    cleanBoard();
+    renderImgs();
+}
+
+function cleanBoard() {
+    document.querySelector('.imgs').innerHTML = '';
+}
+
+
+function makeMeme(elImg) {
+    console.log(elImg);
 }
