aa6d1e7f5ffd2155cb9b6af7d081be531f643348
May <may.schiller@gmail.com>
2017-11-17 15:44:59 +0200

    update sprint2

--- js/main.js
+++ js/main.js
@@ -2,57 +2,75 @@
 
 //scroll functions
 function scrollProtfolio() {
-    window.scrollTo(0, 950)
+    window.scrollTo(0, 950);
 }
 
 function scrollAbout() {
-    window.scrollTo(0, 600)
+    window.scrollTo(0, 600);
 }
 
 function scrollGetTouch() {
-    window.scrollTo(0, 1257)
+    window.scrollTo(0, 1257);
 }
 
 
 
 //render pictures
 var gPics = ['child.jpg', 'baby.jpg', 'girl.jpg', 'woman.jpg', 'man.jpg', 'wanka.jpg', 'american.jpg', 'red.jpg'];
+var gKeyWords = ['child,scary,not-happy', 'baby,strong,success', 'girl,disaster',
+    'woman,crazy', 'man,excuse', 'willy wonka,laugh', 'american,comedy', 'man,extravagant,dont-care'];
 var gImgs = [];
 getImgs();
+var gKeyAppear = {};
+
 
 function getImgs() {
+    
+    var keys = [];
 
     gPics.forEach(function (pic, idx) {
-        console.log('pic', pic);
-        gImgs.push(getImg(pic, idx));
-    })
-    renderImgs();
+        keys = gKeyWords[idx].split(',');
+        
+        keys.forEach(function (key) {
+            console.log(key)
+            getKeyAppear(key);     
+        });
+        gImgs.push(getImg(pic, idx, keys));
+    });
+
+
+    renderImgs(gImgs);
 }
 
+function getKeyAppear(key){
 
-function getImg(pic, idx) {
+    gKeyAppear[key]=0;
+    
+}
+
+function getImg(pic, idx, keys) {
 
     return {
         id: idx,
         url: pic,
-        keywords: []
+        keywords: keys
     }
 }
 
-function renderImgs() {
+function renderImgs(imgs) {
 
     var elImgs = document.querySelector('.imgs');
     var strHtmls = '';
 
-    gImgs.forEach(function (img, idx) {
+    imgs.forEach(function (img, idx) {
         var strHtml = `
-        <div><img  src="img/${img.url}"  id="i-${idx}" onclick="makeMeme(this)" ></div>
+        <div class="hexa-shape"><img  src="img/${img.url}"  id="i-${idx}" onclick="makeMeme(this)" ></div>
         `
         strHtmls += strHtml
     });
     elImgs.innerHTML = strHtmls;
-}
 
+}
 
 function addImg() {
 
@@ -60,12 +78,54 @@ function addImg() {
     gImgs.push(getImg(pic, gPics.length));
     console.log('gPics', gPics);
     cleanBoard();
-    renderImgs();
+    renderImgs(gImgs);
 }
 
 function cleanBoard() {
     document.querySelector('.imgs').innerHTML = '';
 }
+
+
+function showImgByKey() {
+
+    cleanBoard();
+    var isImgs = [];
+
+    var keyValue = document.querySelector('.img-key').value;
+
+    gImgs.forEach(function (img) {
+        img.keywords.forEach(function (key) {
+            if (keyValue === key)
+                isImgs.push(img);
+        });
+    });
+
+    renderImgs(isImgs);
+}
+
+
+
+
+function showList() {
+    cleanBoard();
+
+    var fontSizeBasic = 12;
+    var elClass = document.querySelector('.imgs');
+
+    // for (var i = 0; i < gImgs.length; i++) {
+    //    if(gImgs[i].keywords)
+
+    // }
+
+
+}
+
+
+
+
+
+
+
 
 
 function makeMeme(elImg) {
