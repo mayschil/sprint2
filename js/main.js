9f1af4633315b80216a035fc698a68ba1e4cfc1e
May <may.schiller@gmail.com>
2017-11-18 22:24:21 +0200

    update may

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@
 //render pictures
 var gPics = ['child.jpg', 'baby.jpg', 'girl.jpg', 'woman.jpg', 'man.jpg', 'wanka.jpg', 'american.jpg',
     'red.jpg', 'white-man.jpg', 'dude.jpg', 'de-evil.jpg', 'trump.jpg'];
-var gKeyWords = ['baby,scary,not-happy', 'baby,strong,success', 'girl,disaster',
+var gKeyWords = ['baby,man,not-happy', 'baby,strong,success', 'girl,disaster',
     'woman,crazy', 'baby,excuse', 'willy-wonka,baby', 'american,baby', 'man,extravagant,dont-care',
-    'scary', 'man', 'movie', 'president'];
+    'scary', 'crzay', 'movie', 'president'];
 var gImgs = [];
 getImgs();
 var gKeyAppear = {};
 var gKeySorted = [];
 keyCount();
+var keys = [];
+var gLetters = [];
+
 
 function getImgs() {
 
@@ -62,24 +65,19 @@ function renderImgs(imgs) {
 
 }
 
-function reload() {
+function cleanBoard() {
 
-    document.querySelector('.display-keys').style.height='initial';
-    document.querySelector('.display-keys').style.margin='0';
+    document.querySelector('.display-keys').style.height = 'initial';
+    document.querySelector('.display-keys').style.margin = '0';
     document.querySelector('.display-keys').innerHTML = '';
-    document.querySelector('.img-url').innerHTML = '';
-    document.querySelector('.img-key').value = '';
     var img = document.querySelector('.added-pic');
-    img.src = '';
     img.style.display = 'none';
-
-    // gImgs.pop();
     renderImgs(gImgs);
 }
 
 function addImg() {
 
-    var arr=[];
+    var arr = [];
     var preview = document.querySelector('.added-pic');
     console.log('preview.src', preview)
     var file = document.querySelector('.img-url').files[0];
@@ -92,16 +90,14 @@ function addImg() {
     } else {
         preview.src = "";
     }
-    
+
+    preview.style.display = 'block';
     preview.style.height = '300px';
     preview.style.width = '300px';
-    document.querySelector('.imgs').innerHTML='';
-    
+    document.querySelector('.imgs').innerHTML = '';
+
 }
 
-function cleanBoard() {
-    document.querySelector('.imgs').innerHTML = '';
-}
 
 
 function showImgByKey() {
@@ -119,6 +115,7 @@ function showImgByKey() {
     });
 
     renderImgs(isImgs);
+    document.querySelector('.img-key').value = '';
 }
 
 function showList() {
@@ -142,15 +139,15 @@ function showList() {
 function goToPics(elKey) {
     console.log(elKey.innerHTML)
     var arr = [];
-    for (var i = 0; i < gImgs.length; i++) {
-        for (var j = 0; j < gImgs[i].keywords.length; j++) {
-            if (gImgs[i].keywords[j] === elKey.innerText) {
-                arr.push(gImgs[i]);
-            }
-        }
 
-    }
-    reload();
+    gImgs.forEach(function () {
+        gImgs.gKeyWords.forEach(function () {
+            if (gImgs.gKeyWords === elKey.innerText)
+                arr.push(gImgs[i]);
+        });
+    });
+
+    cleanBoard();
     renderImgs(arr);
 }
 
@@ -168,6 +165,46 @@ function sortKeys() {
     });
 }
 
+function myFunction() {
+
+    var tempWords = '';
+    var str = document.querySelector('.img-key').value;
+    console.log('str', str)
+    gLetters = str.split('');
+    console.log('gLetters', gLetters)
+
+    var arr = ['may', 'mat'];
+
+    for (var i = 0; i < arr.length; i++) {
+        var word = arr[i].split('');
+        for (var k = 0; k < gLetters.length; k++) {
+            if (gLetters[k] === word[k]) {
+                tempWords += gLetters[k];
+            }
+
+            else {
+                console.log('this is not the word');
+                break;
+            }
+        }
+    }
+    console.log('tempWords', tempWords);
+
+    // for (var i = 0; i < gImgs.length; i++) {
+    //     for (var j = 0; j < gImgs[i].keywords.length; j++) {
+    //         keys = gImgs[i].keywords[j].split('');
+    //         for (var k = 0; k < gLetters.length; k++) {
+    //             if (gLetters[k] === keys[k]) {
+    //                 tempWords += gLetters[k];
+    //                 console.log('tempWords', tempWords);
+    //                 k++;
+    //             }
+    //             else break;
+    //         }
+    //     }
+    // }
+    // gLetters = [];
+}
 
 
 
@@ -176,3 +213,6 @@ function sortKeys() {
 function makeMeme(elImg) {
     console.log(elImg);
 }
+
+
+
